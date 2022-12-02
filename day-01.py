8ff2ade91808335077a1d71bc5e2d7ca91a68dbd
Sơn Nguyễn <son@sonng.dev>
2022-12-02 01:29:30 +0000

    feat: Complete day 1 part 2

--- day-01.py
+++ day-01.py
@@ -15,12 +15,20 @@ with request.urlopen(req) as res:
     lines = calories_list.split('\n')
 
     max_sum = 0
+    max_sums = [0, 0, 0]
+
     current_sum = 0
     for line in lines:
         if line == '':
             max_sum = max(current_sum, max_sum)
+
+            max_sums.append(current_sum)
+            max_sums.sort(reverse=True)
+            max_sums.pop()
+
             current_sum = 0
         else:
             current_sum += int(line)
 
-    print(max_sum)
+    print(f'Max calories of one elf: {max_sum}')
+    print(f'Total calories of top three elves: {sum(max_sums)}')
