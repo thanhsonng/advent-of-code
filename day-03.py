3abe1516324772dcb43f29f0e83052ec8b14e442
Sơn Nguyễn <son@sonng.dev>
2022-12-03 13:48:31 +0000

    feat: Complete day 3 part 2

--- day-03.py
+++ day-03.py
@@ -24,6 +24,7 @@ with request.urlopen(req) as res:
     strategy_guide: str = res.read().decode('utf8')
     rucksacks = strategy_guide.split('\n')
 
+    # Part 1
     total_priorities = 0
 
     for rucksack in rucksacks:
@@ -39,3 +40,25 @@ with request.urlopen(req) as res:
                 break
 
     print(f'Total priorities: {total_priorities}')
+
+    # Part 2
+    total_badges_priorities = 0
+    group_counter = 0
+    sets = [set(), set(), set()]
+
+    for rucksack in rucksacks:
+        group_counter += 1
+
+        for item in rucksack:
+            sets[group_counter - 1].add(item)
+
+        if group_counter == 3:
+            badges = sets[0] & sets[1] & sets[2]
+            if len(badges) > 1:
+                raise Exception(f'More than 1 badges found: {badges}')
+            total_badges_priorities += get_priority(badges.pop())
+            for s in sets:
+                s.clear()
+            group_counter = 0
+
+    print(f'Total priorities of badges: {total_badges_priorities}')
