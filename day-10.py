0b1ee785e75f01c8b99bb94f0dbdbcb5e24a88f6
Sơn Nguyễn <son@sonng.dev>
2022-12-10 09:47:29 +0000

    feat: Complete day 10 part 2

--- day-10.py
+++ day-10.py
@@ -1,4 +1,5 @@
 import os
+import math
 from urllib import request
 from dotenv import load_dotenv
 
@@ -18,6 +19,7 @@ with request.urlopen(req) as res:
     all_input: str = res.read().decode('utf8')
     instructions = all_input.strip().split('\n')
 
+    # Part 1
     current_cycle = 1
     x = 1
     total_signal_strength = 0
@@ -44,3 +46,33 @@ with request.urlopen(req) as res:
             x += int(value)
 
     print(f'Total signal strength: {total_signal_strength}')
+
+    # Part 2
+    current_cycle = 1
+    x = 1
+    lines = ['' for _ in range(6)]
+
+    for instruction in instructions:
+        splitted = instruction.split(' ')
+        if len(splitted) == 1:
+            operation = splitted[0]
+        else:
+            operation, value = splitted
+
+        if operation == 'noop':
+            num_cycles = 1
+        elif operation == 'addx':
+            num_cycles = 2
+
+        for i in range(num_cycles):
+            row = math.ceil(current_cycle / 40) - 1
+            line_index = current_cycle - 40 * row
+            lines[row] += '#' if x <= line_index <= x + 2 else '.'
+            current_cycle += 1
+
+        if operation == 'addx':
+            x += int(value)
+
+    print('CRT')
+    for line in lines:
+        print(line)
